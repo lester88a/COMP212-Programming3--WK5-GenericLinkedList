62e1148bc8a8aca60910269083d05696c2568d9f
lester88a <lester88a@gmail.com>
2015-10-09 15:28:10 -0400

    Test efficiency

--- GenericLinkedList/Program.cs
+++ GenericLinkedList/Program.cs
@@ -10,6 +10,39 @@ namespace GenericLinkedList
     {
         static void Main(string[] args)
         {
+            //creates a linked list
+            LinkedList<string> linked1 = new LinkedList<string>();
+            
+            //appending three elements to the list
+            linked1.AddLast("One");
+            linked1.AddLast("Two");
+            linked1.AddLast("Three");
+            linked1.AddFirst("Zero");
+            PrintLinked1(linked1);
+
+            Console.WriteLine();
+            //insterting a node between element "Two" and "Three"
+            LinkedListNode<string> myNode = linked1.Find("Three");
+            linked1.AddBefore(myNode, "Inserted using add before.");
+            PrintLinked1(linked1);
+
+            //removing one element
+            linked1.Remove("One");
+            PrintLinked1(linked1);
+
+            //remove Three using myNode
+            myNode = linked1.Find("Three");
+            linked1.Remove(myNode);
+            PrintLinked1(linked1);
+        }
+
+        private static void PrintLinked1(LinkedList<string> linked1)
+        {
+            Console.WriteLine();
+            foreach (var item in linked1)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
---
+++ TestEfficiency/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestEfficiency
+{
+    class Program
+    {
+        const int MAX = 10000;
+        static void Main(string[] args)
+        {
+            //creating a list and a linked list
+            var list = new List<string>();
+            var link = new LinkedList<string>();
+
+            //adding a elements to both
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add("OK");
+                link.AddLast("OK");
+            }
+
+
+
+            var s1 = Stopwatch.StartNew();
+            for (int i = 0; i < MAX; i++)
+            {
+                foreach (string v in list)
+                {
+                    if (v.Length != 2)
+                    {
+                        throw new Exception();
+                    }
+                }                
+            }
+            s1.Stop();
+
+
+            var s2 = Stopwatch.StartNew();
+            for (int i = 0; i < MAX; i++)
+            {
+                foreach (string v2 in link)
+                {
+                    if (v2.Length!=2)
+                    {
+                        throw new Exception();
+                    }
+                }
+                
+            }
+            s2.Stop();
+            Console.WriteLine( ((double)(s1.Elapsed.TotalMilliseconds*1000000) / MAX).ToString("0.00 ns") );
+            Console.WriteLine(((double)(s2.Elapsed.TotalMilliseconds * 1000000) / MAX).ToString("0.00 ns"));
+
+        }
+    }
+}
